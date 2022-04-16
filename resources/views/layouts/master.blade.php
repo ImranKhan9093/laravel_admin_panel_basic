55e4341f82c2039753e1203866e88744cf515389
Imran Khan <imrankhan96799@gmail.com>
2022-04-16 20:00:51 +0530

    add datatables feature with search box and paginatiion to admin/aboutus view.Add datatbables css and js for this feature in public assests directory

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -34,6 +34,7 @@
   <link href="{{ URL('assets/css/now-ui-dashboard.css?v=1.5.0') }}" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
   <link href="{{ URL('assets/demo/demo.css') }}" rel="stylesheet" />
+  <link rel="stylesheet" href="{{ URL('assets/css/dataTables.min.css') }}">
 </head>
 
 <body class="">
@@ -210,19 +211,21 @@
     </div>
   </div>
   <!--   Core JS Files   -->
-  <script src="../assets/js/core/jquery.min.js"></script>
-  <script src="../assets/js/core/popper.min.js"></script>
-  <script src="../assets/js/core/bootstrap.min.js"></script>
-  <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <script src="{{ URL('assets/js/core/jquery.min.js') }}"></script>
+  <script src="{{ URL('assets/js/core/popper.min.js') }}"></script>
+  <script src="{{ URL('assets/js/core/bootstrap.min.js') }}"></script>
+  <script src="{{ URL('assets/js/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
+  <script src="{{ URL('assets/js/dataTables.min.js') }}"></script>
   <!--  Google Maps Plugin    -->
   <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
   <!-- Chart JS -->
-  <script src="../assets/js/plugins/chartjs.min.js"></script>
+  <script src="{{ URL('assets/js/plugins/chartjs.min.js') }}"></script>
   <!--  Notifications Plugin    -->
-  <script src="../assets/js/plugins/bootstrap-notify.js"></script>
+  <script src="{{ URL('assets/js/plugins/bootstrap-notify.js') }}"></script>
   <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="../assets/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script><!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
-  <script src="../assets/demo/demo.js"></script>
+  <script src="{{ URL('assets/js/now-ui-dashboard.min.js?v=1.5.0') }}" type="text/javascript"></script><!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
+  <script src="{{ URL('assets/demo/demo.js') }}"></script>
+  
 
   @yield('scripts')
 
